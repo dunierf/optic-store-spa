0aeb27b82c7a86bed615e737393e17172ebc2ed1
Duniesqui Rodriguez <dunierf@gmail.com>
2026-06-13 23:18:52 -0500

    Footer

--- src/app/components/layout/layout/layout.ts
+++ src/app/components/layout/layout/layout.ts
@@ -4,13 +4,15 @@ import { Component } from '@angular/core';
 import { TopToolbar } from '../top-toolbar/top-toolbar';
 import { AfterTopToolbarArea } from '../after-top-toolbar-area/after-top-toolbar-area';
 import { ContentArea } from '../content-area/content-area';
+import { Footer } from '../footer/footer';
 
 @Component({
   selector: 'app-layout',
   imports: [
     TopToolbar,
     AfterTopToolbarArea,
-    ContentArea
+    ContentArea,
+    Footer
   ],
   templateUrl: './layout.html',
   styleUrl: './layout.css',
